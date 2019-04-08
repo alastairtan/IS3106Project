4aab37efac08bbc4ed3721064b3886117ecaf818
E0175613 <E0175613@u.nus.edu>
2019-04-09 05:25:48 +0800

    Update customer profile

--- StoreCraftCustomers/src/app/user-profile/user-profile.component.ts
+++ StoreCraftCustomers/src/app/user-profile/user-profile.component.ts
@@ -15,18 +15,19 @@ export class UserProfileComponent implements OnInit {
 
   isUpdating: boolean;
 
-  bronzeTierUrl: string;
+  tierUrl: string;
+  tierMessage: string;
 
   constructor(
     public sessionService: SessionService
   ) {
     this.isUpdating = false;
-    this.bronzeTierUrl = "https://opgg-static.akamaized.net/images/medals/bronze_1.png";
    }
 
   ngOnInit() {
     this.customer = this.sessionService.getCurrentCustomer();
     this.customerMemTierString = MembershipTierEnum[this.customer.membershipTierEnum];
+    this.setTierInfo();
     console.log(this.customer.profilePicUrl);
     console.log(MembershipTierEnum[this.customer.membershipTierEnum]);
   }
@@ -37,6 +38,50 @@ export class UserProfileComponent implements OnInit {
 
   cancelUpdating(){
     this.isUpdating = false;
+  }
+
+  saveChanges(){
+
+  }
+
+  setTierInfo(){
+    switch(this.customer.membershipTierEnum){
+      case 0: {
+        this.tierMessage = "Welcome, Bronzie!";
+        this.tierUrl = "https://opgg-static.akamaized.net/images/medals/bronze_1.png";
+        break;
+      }
+      case 1: {
+        this.tierMessage = "Every cloud has a Silver lining!";
+        this.tierUrl = "https://opgg-static.akamaized.net/images/medals/silver_1.png";
+        break;
+      }
+      case 2: {
+        this.tierMessage = "AU YEAH!!! Gold! ";
+        this.tierUrl = "https://opgg-static.akamaized.net/images/medals/gold_1.png";
+        break;
+      }
+      case 3: {
+        this.tierMessage = "Platinum means you're halfway there!";
+        this.tierUrl = "https://opgg-static.akamaized.net/images/medals/platinum_1.png";
+        break;
+      }
+      case 4: {
+        this.tierMessage = "Shine bright like a Diamond!";
+        this.tierUrl = "https://opgg-static.akamaized.net/images/medals/diamond_1.png";
+        break;
+      }
+      case 5: {
+        this.tierMessage = "Just a little more to go, Master!";
+        this.tierUrl = "https://opgg-static.akamaized.net/images/medals/master_1.png";
+        break;
+      }
+      case 6: {
+        this.tierMessage = "RAMPAGEEE!! Congratulations, Grandmaster!";
+        this.tierUrl = "https://opgg-static.akamaized.net/images/medals/grandmaster_1.png";
+        break;
+      }
+    }
   }
 
 }
