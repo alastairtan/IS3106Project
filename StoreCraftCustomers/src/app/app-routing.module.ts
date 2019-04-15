00f3f062a16fa9e82f864409a7092299ac03cd53
winphong <winphong@u.nus.edu>
2019-04-16 01:54:53 +0800

    expandable table

--- StoreCraftCustomers/src/app/app-routing.module.ts
+++ StoreCraftCustomers/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ViewProductDetailsComponent } from './view-product-details/view-product-details.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CommunityGoalsComponent } from './community-goals/community-goals.component';
+import { SaleTransactionHistoryComponent } from './sale-transaction-history/sale-transaction-history.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/index', pathMatch: 'full' },
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path: 'viewProductsByCategory/:categoryId', component: ViewProductsComponent },
   { path: 'product/:productId', component: ViewProductDetailsComponent},
   { path: 'cart/:userId', component: ShoppingCartComponent},
-  { path: 'communitygoals', component: CommunityGoalsComponent}
+  { path: 'communitygoals', component: CommunityGoalsComponent},
+  { path: 'saleTransactionHistory', component: SaleTransactionHistoryComponent}
 ];
 
 @NgModule({
--- StoreCraftCustomers/src/app/sale-transaction-history/sale-transaction-history.component.ts
+++ StoreCraftCustomers/src/app/sale-transaction-history/sale-transaction-history.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { SaleTransactionService } from '../sale-transaction.service';
 import { SaleTransaction } from '../saleTransaction';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-sale-transaction-history',
   templateUrl: './sale-transaction-history.component.html',
-  styleUrls: ['./sale-transaction-history.component.css']
+  styleUrls: ['./sale-transaction-history.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class SaleTransactionHistoryComponent implements OnInit {
 
   private saleTransactions: SaleTransaction[];
+  private columnsToDisplay = ['saleTransactionId', 'totalLineItem', 'Total Quantity', 'totalAmount'];
+  private expandedSaleTransaction; 
 
   constructor(private saleTransactionService: SaleTransactionService,
   ) { }
 
   ngOnInit() {
     this.saleTransactionService.retrieveAllTransactions().subscribe(response => {
-      this.saleTransactions = response;
+      this.saleTransactions = response.saleTransactionEntities;
     }
     )
   }
