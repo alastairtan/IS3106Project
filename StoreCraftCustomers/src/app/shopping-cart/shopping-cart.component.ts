5e13a1b2bcc1e7004f5c29919ef2ae9d3a1c0ef5
E0175613 <E0175613@u.nus.edu>
2019-04-15 21:39:20 +0800

    Update shopping cart UI, add change quantity function

--- StoreCraftCustomers/src/app/shopping-cart/shopping-cart.component.ts
+++ StoreCraftCustomers/src/app/shopping-cart/shopping-cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CartItem } from '../cartItem';
 import { LocalService } from '../local.service';
 import { SaleTransactionService } from '../sale-transaction.service';
+import { refreshDescendantViews } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -61,10 +62,30 @@ export class ShoppingCartComponent implements OnInit {
 
     this.removeMessageClose = true;
     this.infoMessageClose = false;
-    this.infoMessage = "Cart successfully checked out!";
+    this.infoMessage = "Thank you for shopping with us!";
     this.localService.clearCart();
     this.cartItems = this.localService.getCart();
 
-    setTimeout(() => this.infoMessageClose = true, 3000);
+    //setTimeout(() => this.infoMessageClose = true, 3000);
   }
+
+  add(cartItem: CartItem){
+    cartItem.quantity = cartItem.quantity + 1;
+    cartItem.subTotal = cartItem.quantity * cartItem.unitPrice;
+    this.refresh();
+  }
+
+  minus(cartItem: CartItem){
+    cartItem.quantity = cartItem.quantity - 1;
+    cartItem.subTotal = cartItem.quantity * cartItem.unitPrice;
+    this.refresh();
+  }
+
+  refresh(){
+    this.localService.setCart(this.cartItems);
+    this.cartItems = this.localService.getCart();
+    this.totalAmountForTheCart = this.cartItems.reduce((acc, cartItem) => acc + cartItem.subTotal, 0);
+    this.totalQuantityForTheCart = this.cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0);
+  }
+  
 }
