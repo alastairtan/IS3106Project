91bc11eb81b3a6193e112ca80848991a802007e0
E0175613 <E0175613@u.nus.edu>
2019-04-18 04:27:44 +0800

    Update community goals search function

--- StoreCraftCustomers/src/app/community-goals/community-goals.component.ts
+++ StoreCraftCustomers/src/app/community-goals/community-goals.component.ts
@@ -1,10 +1,9 @@
-
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatSort, MatPaginator, MatDialog, MatDialogRef} from '@angular/material';
-import { SessionService } from '../session.service';
-import { CommunityGoalService } from '../community-goal.service';
-import {CommunityGoal} from '../community-goal'
-import { CountryService } from '../country.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource, MatSort, MatPaginator, MatDialog, MatDialogRef} from '@angular/material';
+import {SessionService} from '../session.service';
+import {CommunityGoalService} from '../community-goal.service';
+import {CommunityGoal} from '../community-goal';
+import {CountryService} from '../country.service';
 
 @Component({
   selector: 'app-community-goals',
@@ -18,67 +17,63 @@ export class CommunityGoalsComponent implements OnInit {
 
   public dataSource = new MatTableDataSource<CommunityGoal>();
   */
-  country : String;
+  country: String;
   countries;
-  communityGoals : CommunityGoal[];
-  errorMessage : String;
-  originalCommunityGoals : CommunityGoal[];
+  communityGoals: CommunityGoal[];
+  errorMessage: String;
+  originalCommunityGoals: CommunityGoal[];
 
 
-  constructor(public sessionService : SessionService,
-              public communityGoalService : CommunityGoalService,
-              public countryService : CountryService,
-              public dialog : MatDialog) { }
+  constructor(public sessionService: SessionService,
+              public communityGoalService: CommunityGoalService,
+              public countryService: CountryService,
+              public dialog: MatDialog) {
+  }
 
   ngOnInit() {
-      this.communityGoalService.retrieveAllCommunityGoals().subscribe(
-      response =>{
+    this.communityGoalService.retrieveAllCommunityGoals().subscribe(
+      response => {
         this.communityGoals = response.communityGoalEntities;
         this.originalCommunityGoals = this.communityGoals;
-        for(let communityGoal of this.communityGoals){
-          if(this.communityGoals.length != 0) {
-            
-            //communityGoal.targetPoints = (communityGoal.currentPoints*100)/communityGoal.targetPoints;
+        for (const communityGoal of this.communityGoals) {
+          if (this.communityGoals.length != 0) {
+
+            // communityGoal.targetPoints = (communityGoal.currentPoints*100)/communityGoal.targetPoints;
             console.log('********** CommunityGoalComponent.ts: ' + communityGoal.rewardPercentage);
           }
+        }
       }
-      }
-      ,error =>{
+      , error => {
         console.log('********** CommunityGoalComponent.ts: ', error);
       }
-    )
-      this.countryService.getCountries().subscribe(
-        response =>{
-          this.countries = response;
-        }, error =>{
-          console.log('********** Getting countries in communitygoal: ', error);
-        }
-      )
-  }
-
-
-  public doFilter (value: string) {
-    console.log("whats in here : " + value);
-    let temp = [];
-    if(this.communityGoals.length != 0) {
-    for(let communityGoal of this.communityGoals){
-      if(communityGoal.country.includes(value)) {
-        
-        temp.push(communityGoal);
+    );
+    this.countryService.getCountries().subscribe(
+      response => {
+        this.countries = response;
+      }, error => {
+        console.log('********** Getting countries in communitygoal: ', error);
       }
-    }
-    console.log(temp);
-    if(temp.length === 0){
-      this.errorMessage = "No half measures, type the exact wording";
-      this.communityGoals = this.originalCommunityGoals;
-    }else{
-      this.errorMessage = null;
-    this.communityGoals = temp;
-  }
-}
+    );
   }
 
-  resetView(){
+
+  public doFilter(value: string) {
+
+    const temp = [];
+    if (this.communityGoals.length != 0 && value.length > 0) {
+      for (const communityGoal of this.communityGoals) {
+        if (communityGoal.country.toLowerCase().includes(value.toLowerCase())) {
+
+          temp.push(communityGoal);
+        }
+      }
+      this.communityGoals = temp;
+    } else {
+      this.communityGoals = this.originalCommunityGoals;
+    }
+  }
+
+  resetView() {
     this.communityGoals = this.originalCommunityGoals;
   }
 
